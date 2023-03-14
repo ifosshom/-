2d71ee59bf5f8fc2f926199f54d684a315c8f000
Ivan Burdukowsky <ivan.burdukowsky@gmail.com>
2023-03-14 20:32:45 +0700

    alert заработал, не хватало кавычек

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
+"use strict"
+
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
-"use strict";
 
 let valid = true;
 let answer = prompt("Любишь yungrussia?", "");
-if (answer == да || answer == "") {
+if ('answer == да' ) {
   alert("Ты не ответил да!");
   valid = false;
 }
