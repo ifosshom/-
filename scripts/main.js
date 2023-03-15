e7486cef9c6d1c852daefad977b75daa4fc8bd58
Ivan Burdukowsky <ivan.burdukowsky@gmail.com>
2023-03-15 22:57:49 +0700

    now, say yor name

--- scripts/main.js
+++ scripts/main.js
@@ -10,5 +10,21 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/yr.jpg');
     }
 }
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
 
+function setUserName() {
+  var myName = prompt('Ты кто??????');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Спасибо твоим родителям за то что назвали тебя ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Спасибо твоим родителям за то что назвали тебя ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
 
